4f208c074bd6894c76ff8fe15064d6c2476fa6be
enerush <nyw077@gmail.com>
2022-08-26 16:40:51 +0300

    New feats

---
+++ Module 1/1.5/1.5.4.py
@@ -0,0 +1,48 @@
+"""
+Подвиг 4. Объявите три класса геометрических фигур: Line, Rect, Ellipse. Должна быть возможность создавать объекты каждого класса следующими командами:
+
+g1 = Line(a, b, c, d)
+g2 = Rect(a, b, c, d)
+g3 = Ellipse(a, b, c, d)
+Здесь в качестве аргументов a, b, c, d передаются координаты верхнего правого и нижнего левого углов (произвольные числа). В каждом объекте координаты должны сохраняться в локальных свойствах sp (верхний правый угол) и ep (нижний левый) в виде кортежей (a, b) и (c, d) соответственно.
+
+Сформируйте 217 объектов этих классов: для каждого текущего объекта класс выбирается случайно (или Line, или Rect, или Ellipse). Координаты также генерируются случайным образом (числовые значения). Все объекты сохраните в списке elements.
+
+В списке elements обнулите координаты объектов только для класса Line.
+
+P.S. На экран в программе ничего выводить не нужно.
+"""
+
+
+from random import choices, choice
+
+
+class Line:
+    def __init__(self, a=0, b=0, c=0, d=0):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+class Rect:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+class Ellipse:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+elements = []
+funcs = [Line, Rect, Ellipse]
+nums = list(range(1, 10))
+
+for i in range(217):
+    func = choice(funcs)
+    a, b, c, d = list(choices(nums, k=4))
+    if func == Line:
+        elements.append(func())
+    else:
+        elements.append(func(a, b, c, d))
---
+++ Module 1/1.5/1.5.5.py
@@ -0,0 +1,39 @@
+"""Подвиг 5. Объявите класс TriangleChecker, объекты которого можно было бы создавать командой:
+
+tr = TriangleChecker(a, b, c)
+Здесь a, b, c - длины сторон треугольника.
+
+В классе TriangleChecker необходимо объявить метод is_triangle(), который бы возвращал следующие коды:
+
+1 - если хотя бы одна сторона не число (не float или int) или хотя бы одно число меньше или равно нулю;
+2 - указанные числа a, b, c не могут являться длинами сторон треугольника;
+3 - стороны a, b, c образуют треугольник.
+
+Проверку параметров a, b, c проводить именно в таком порядке.
+
+Прочитайте из входного потока строку, содержащую три числа, разделенных пробелами, командой:
+
+a, b, c = map(int, input().split())
+Затем, создайте объект tr класса TriangleChecker и передайте ему прочитанные значения a, b, c.
+Вызовите метод is_triangle() из объекта tr и выведите результат на экран (код, который она вернет).
+"""
+
+
+class TriangleChecker:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def is_triangle(self):
+        if type(self.a) not in (float, int) or type(self.b) not in (float, int) or \
+                type(self.c) not in (float, int) or 0 in (self.a, self.b, self.c):
+            return 1
+        if self.a + self.b < self.c or self.a + self.c < self.c or self.b + self.c < self.a:
+            return 2
+        return 3
+
+
+a, b, c = map(int, input().split())  # эту строчку не менять
+tr = TriangleChecker(a, b, c)
+print(TriangleChecker.is_triangle(tr))
